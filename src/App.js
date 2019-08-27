2bb2abb8e182d3b9e17d62bc5985dd8f6b96ca25
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2019-08-27 17:44:37 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Route from 'react-router-dom';
 import HomePage from './pages/homepage/hompage.component';
 
+const HatsPage = () => (
+	<div>
+		<h1>HATS PAGE</h1>
+	</div>
+);
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Route exect path='/' component={HomePage} />
+			<Route exect path='/hats' component={HatsPage} />
 		</div>
 	);
 }
