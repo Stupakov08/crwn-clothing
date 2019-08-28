3a74f919e80238007d5dd5981cc9b7e0b8a28b48
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2019-08-28 15:23:16 +0300

    adding hedaer component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.components';
+import Header from './components/header/header.component';
 
 function App() {
 	return (
 		<div>
+			<Header></Header>
 			<Switch>
 				<Route path='/shop' component={ShopPage} />
 				<Route path='/' component={HomePage} />
