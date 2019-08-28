4c1b616f2d35a2d6efed887f93bb87bb058ce38a
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2019-08-28 17:16:16 +0300

    Add Sign in Page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.components';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
 	return (
 		<div>
 			<Header></Header>
 			<Switch>
+				<Route path='/signin' component={SignInAndSignUpPage} />
 				<Route path='/shop' component={ShopPage} />
 				<Route path='/' component={HomePage} />
 			</Switch>
