2fa1026dc65f40436f6cb311d170876e12c38130
Stupakov08 <stupakov08@gmail.com>
2019-08-28 23:11:52 +0300

    finished sign-in form

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./form-input.styles.scss";
+
+const FormInput = ({ handleChange, label, ...rest }) => (
+  <div className='group'>
+    <input
+      type='text'
+      className='form-input'
+      onChange={handleChange}
+      {...rest}
+    />
+    {label ? (
+      <label
+        className={`${rest.value.length ? "shrink" : ""} form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+export default FormInput;
