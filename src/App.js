224125048954fb39d34775598b21ca99af01f2d9
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2019-09-03 10:27:46 +0300

    implementing firebase utils

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.components';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 	constructor() {
@@ -18,10 +18,21 @@ class App extends React.Component {
 	unsubscibeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+			if (user) {
+				const userRef = await createUserProfileDocument(user);
 
-			console.log(user);
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+				});
+				console.log(this.state);
+			}
+			this.setState({ currentUser: user });
 		});
 	}
 	componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,29 @@ const config = {
 	appId: '1:135180319982:web:9b9bb97f185a6a4a'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapshot = await userRef.get();
+	if (!snapshot.exists) {
+		const { displayName, email } = userAuth;
+		const createAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error create user', error.message);
+		}
+	}
+
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
