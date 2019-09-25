1f61461acd07bd3e45d1627656a223ac918d3498
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2019-09-25 14:01:00 +0300

    used reselect

--- src/components/cart-icon/cart-icon.components.js
+++ src/components/cart-icon/cart-icon.components.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
 	<div className='cart-icon' onClick={toggleCartHidden}>
@@ -17,11 +18,8 @@ const mapDispatchToProps = dispatch => ({
 	}
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-	itemCount: cartItems.reduce(
-		(accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-		0
-	)
+const mapStateToProps = state => ({
+	itemCount: selectCartItemsCount(state)
 });
 
 export default connect(
