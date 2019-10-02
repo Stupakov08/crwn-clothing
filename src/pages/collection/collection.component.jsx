a84c384cec5c820d41df5a1141e669fe3a5d3020
Stupakov08 <stupakov08@gmail.com>
2019-10-02 23:31:22 +0300

    collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './collection.styles.scss';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+const CollectionPage = ({ match }) => (
+	<div className='ccollection-page'>
+		<h2>Category page</h2>
+	</div>
+);
+
+export default CollectionPage;
