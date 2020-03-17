19141a3605bd1d69d44abe2bcd9c77ebdff83fd5
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2020-03-17 21:00:56 +0200

    React optimization

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { GlobalStyle } from './global.styles';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import HomePage from './pages/homepage/hompage.component';
-import ShopPage from './pages/shop/shop.components';
 import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 import { selectCollectionsForPreview } from './redux/shop/shop.selector'
-import CheckoutPage from './pages/checkout/checkout.component';
+import Spinner from './components/spinner/spinner.component'
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+
+const HomePage = lazy(() => import('./pages/homepage/hompage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.components'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
 
 const App = ({ checkUserSession, currentUser }) => {
 	useEffect(() => {
@@ -23,20 +26,24 @@ const App = ({ checkUserSession, currentUser }) => {
 			<GlobalStyle />
 			<Header></Header>
 			<Switch>
-				<Route exact path='/' component={HomePage} />
-				<Route path='/shop' component={ShopPage} />
-				<Route exact path='/checkout' component={CheckoutPage} />
-				<Route
-					exact
-					path='/signin'
-					render={() =>
-						currentUser ? (
-							<Redirect to='/' />
-						) : (
-								<SignInAndSignUpPage />
-							)
-					}
-				/>
+				<ErrorBoundary>
+					<Suspense fallback={<Spinner />}>
+						<Route exact path='/' component={HomePage} />
+						<Route path='/shop' component={ShopPage} />
+						<Route exact path='/checkout' component={CheckoutPage} />
+						<Route
+							exact
+							path='/signin'
+							render={() =>
+								currentUser ? (
+									<Redirect to='/' />
+								) : (
+										<SignInAndSignUpPage />
+									)
+							}
+						/>
+					</Suspense>
+				</ErrorBoundary>
 			</Switch>
 		</div>
 	);
--- client/src/pages/shop/shop.components.jsx
+++ client/src/pages/shop/shop.components.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
-import CollectionOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
-import CollectionPageContainer from '../collection/collection.container';
 import { connect } from 'react-redux';
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+import Spinner from '../../components/spinner/spinner.component';
+
+const CollectionOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
 
 const ShopPage = ({ match, fetchCollectionsStart }) => {
 	useEffect(() => {
@@ -12,8 +14,10 @@ const ShopPage = ({ match, fetchCollectionsStart }) => {
 
 	return (
 		<div className='shop-page'>
-			<Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
-			<Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+			<Suspense fallback={<Spinner />}>
+				<Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
+				<Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+			</Suspense>
 		</div>
 	);
 };
